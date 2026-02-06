fb7852046f2370c9c13744308bcc367f94fbfd59
prashikubale <prashik2927@gmail.com>
2026-02-06 18:52:19 +0530

    Fix dashboard 404 issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: './', // Makes paths relative for easier deployment/viewing
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dashboard: resolve(__dirname, 'dashboard.html'),
+            }
+        }
+    },
     server: {
         host: true, // Listen on all addresses
         open: true, // Attempt to open in browser automatically
